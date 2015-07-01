a540b080946bf0ea457ffe1ce5dcf0cb5c84df8b
JinfengChen <jinfeng7chen@gmail.com>
2015-07-01 11:42:40 -0700

    asm40

--- Summary/Read_Length_Plot/work.sh
+++ Summary/Read_Length_Plot/work.sh
@@ -9,3 +9,18 @@ perl CompareReadLength.py --list Citrus/input.16.fofn --project Pacbio_Citrus_16
 python SummaryLibReadLength.py --list Citrus/input.16.fofn --project Pacbio_Citrus_16 > log 2>&1 &
 python SummaryLibReadLength.py --list Citrus/input.fofn --project Pacbio_Citrus_8 > log 2>&1 &
 
+echo "Citrus third 12 SMRT cell"
+perl CompareReadLength.py --list Citrus/input.12.fofn --project Pacbio_Citrus_12
+python SummaryLibReadLength.py --list Citrus/input.12.fofn --project Pacbio_Citrus_12 > log 2>&1 &
+
+echo "Citrus forth 4 SMRT cell"
+perl CompareReadLength.py --list Citrus/input.4.fofn --project Pacbio_Citrus_4
+python SummaryLibReadLength.py --list Citrus/input.4.fofn --project Pacbio_Citrus_4 > log 2>&1 &
+
+echo "Corrected reads 36 SMRT cell"
+perl CompareReadLength.py --fasta Citrus/preads4falcon.fasta --list Citrus/input.36.fofn --project Pacbio_Citrus_36
+perl CompareReadLength.py --list Citrus/input_preads.fofn --project Pacbio_Citrus_36_preads
+
+echo "Corrected read 40 SMRT cell"
+perl CompareReadLength.py --fasta Citrus/preads4falcon.fasta --list Citrus/input.40.fofn --project Pacbio_Citrus_40
+
